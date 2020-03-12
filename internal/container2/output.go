a4202879c5874f8f4ae34dd6d054e4da4e5d89e9
Vlad <vilgovskiy@densify.com>
2020-03-12 10:55:55 -0400

    Refactor output.go

--- internal/container2/output.go
+++ internal/container2/output.go
@@ -121,14 +121,15 @@ func writeAttributes(clusterName, promAddr string) string {
 				fmt.Fprintf(attributeWrite, ",")
 
 				for key, value := range vt.labelMap {
-					if len(key) < 250 {
-						value = strings.Replace(value, ",", " ", -1)
-						if len(value)+3+len(key) < 256 {
-							fmt.Fprintf(attributeWrite, key+" : "+value+"|")
-						} else {
-							templength := 256 - 3 - len(key)
-							fmt.Fprintf(attributeWrite, key+" : "+value[:templength]+"|")
-						}
+					if len(key) >= 250 {
+						continue
+					}
+					value = strings.Replace(value, ",", " ", -1)
+					if len(value)+3+len(key) < 256 {
+						fmt.Fprintf(attributeWrite, key+" : "+value+"|")
+					} else {
+						templength := 256 - 3 - len(key)
+						fmt.Fprintf(attributeWrite, key+" : "+value[:templength]+"|")
 					}
 				}
 
@@ -137,7 +138,7 @@ func writeAttributes(clusterName, promAddr string) string {
 				} else {
 					fmt.Fprintf(attributeWrite, ",%d", vc.cpuLimit)
 				}
-				if vc.cpuLimit == -1 {
+				if vc.cpuRequest == -1 {
 					fmt.Fprintf(attributeWrite, ",")
 				} else {
 					fmt.Fprintf(attributeWrite, ",%d", vc.cpuRequest)
@@ -164,22 +165,25 @@ func writeAttributes(clusterName, promAddr string) string {
 					//Formatting the date into the expexted format. Note the reason for that date is a Go specific way of declaring a format you must use that exact date and time.
 					fmt.Fprintf(attributeWrite, ",%s", time.Unix(int64(vt.creationTime), 0).Format("2006-01-02 15:04:05.000"))
 				}
+				// TODO: Possibly an extra comma
 				if vc.restarts == -1 {
 					fmt.Fprintf(attributeWrite, ",,")
 				} else {
 					fmt.Fprintf(attributeWrite, ",%d,", vc.restarts)
 				}
 				for key, value := range vn.labelMap {
-					if len(key) < 250 {
-						value = strings.Replace(value, ",", " ", -1)
-						if len(value)+3+len(key) < 256 {
-							fmt.Fprintf(attributeWrite, key+" : "+value+"|")
-						} else {
-							templength := 256 - 3 - len(key)
-							fmt.Fprintf(attributeWrite, key+" : "+value[:templength]+"|")
-						}
+					if len(key) >= 250 {
+						continue
+					}
+					value = strings.Replace(value, ",", " ", -1)
+					if len(value)+3+len(key) < 256 {
+						fmt.Fprintf(attributeWrite, key+" : "+value+"|")
+					} else {
+						templength := 256 - 3 - len(key)
+						fmt.Fprintf(attributeWrite, key+" : "+value[:templength]+"|")
 					}
 				}
+				// TODO: Not sure but order of these writes is different. Neet to check file format.
 				if vn.cpuRequest == -1 {
 					fmt.Fprintf(attributeWrite, ",")
 				} else {
@@ -246,71 +250,86 @@ func writeHPAAttributes(clusterName, promAddr string, systems map[string]map[str
 //writeWorkload will write out the workload data specific to metric provided to the file that was passed in.
 func writeWorkload(file io.Writer, result model.Value, namespace, pod, container model.LabelName, clusterName, promAddr string, kind string) {
 	var tempKind bool
-	if result != nil {
-		if kind == "" {
-			tempKind = true
+	if result == nil {
+		return
+	}
+	if kind == "" {
+		tempKind = true
+	}
+	//Check if the cluster parameter is set and if it is then use it for the name of the cluster if not use the prometheus address as the cluster name.
+	var cluster string
+	if clusterName == "" {
+		cluster = promAddr
+	} else {
+		cluster = clusterName
+	}
+	//Loop through the results for the workload and validate that contains the required labels and that the entity exists in the systems data structure once validated will write out the workload for the system.
+	for i := 0; i < result.(model.Matrix).Len(); i++ {
+		if tempKind {
+			kind = string(result.(model.Matrix)[i].Metric["owner_kind"])
 		}
-		//Check if the cluster parameter is set and if it is then use it for the name of the cluster if not use the prometheus address as the cluster name.
-		var cluster string
-		if clusterName == "" {
-			cluster = promAddr
-		} else {
-			cluster = clusterName
+		namespaceValue, ok := result.(model.Matrix)[i].Metric[namespace]
+		if !ok {
+			continue
 		}
-		//Loop through the results for the workload and validate that contains the required labels and that the entity exists in the systems data structure once validated will write out the workload for the system.
-		for i := 0; i < result.(model.Matrix).Len(); i++ {
-			if tempKind {
-				kind = string(result.(model.Matrix)[i].Metric["owner_kind"])
-			}
-			if namespaceValue, ok := result.(model.Matrix)[i].Metric[namespace]; ok {
-				if _, ok := systems[string(namespaceValue)]; ok {
-					if podValue, ok := result.(model.Matrix)[i].Metric[pod]; ok {
-						if _, ok := systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)]; ok {
-							if containerValue, ok := result.(model.Matrix)[i].Metric[container]; ok {
-								if _, ok := systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].containers[string(containerValue)]; ok {
-									//Loop through the different values over the interval and write out each one to the workload file.
-									for j := 0; j < len(result.(model.Matrix)[i].Values); j++ {
-										fmt.Fprintf(file, "%s,%s,%s,%s,%s,%s,%f\n", cluster, namespaceValue, systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].name, systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].kind, strings.Replace(string(containerValue), ":", ".", -1), time.Unix(0, int64(result.(model.Matrix)[i].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"), result.(model.Matrix)[i].Values[j].Value)
-									}
-								}
-							}
-						}
-					}
-				}
-			}
+		if _, ok := systems[string(namespaceValue)]; !ok {
+			continue
+		}
+		podValue, ok := result.(model.Matrix)[i].Metric[pod]
+		if !ok {
+			continue
+		}
+		if _, ok := systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)]; !ok {
+			continue
+		}
+		containerValue, ok := result.(model.Matrix)[i].Metric[container]
+		if !ok {
+			continue
+		}
+		if _, ok := systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].containers[string(containerValue)]; !ok {
+			continue
+		}
+		//Loop through the different values over the interval and write out each one to the workload file.
+		for j := 0; j < len(result.(model.Matrix)[i].Values); j++ {
+			fmt.Fprintf(file, "%s,%s,%s,%s,%s,%s,%f\n", cluster, namespaceValue, systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].name, systems[string(namespaceValue)].midLevels[kind+"__"+string(podValue)].kind, strings.Replace(string(containerValue), ":", ".", -1), time.Unix(0, int64(result.(model.Matrix)[i].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"), result.(model.Matrix)[i].Values[j].Value)
 		}
 	}
 }
 
 //writeWorkload will write out the workload data specific to metric provided to the file that was passed in.
 func writeWorkloadMid(file io.Writer, result model.Value, namespace, mid model.LabelName, clusterName, promAddr string, prefix string) {
-	if result != nil {
-		//Check if the cluster parameter is set and if it is then use it for the name of the cluster if not use the prometheus address as the cluster name.
-		var cluster string
-		if clusterName == "" {
-			cluster = promAddr
-		} else {
-			cluster = clusterName
+	if result == nil {
+		return
+	}
+	//Check if the cluster parameter is set and if it is then use it for the name of the cluster if not use the prometheus address as the cluster name.
+	var cluster string
+	if clusterName == "" {
+		cluster = promAddr
+	} else {
+		cluster = clusterName
+	}
+	//Loop through the results for the workload and validate that contains the required labels and that the entity exists in the systems data structure once validated will write out the workload for the system.
+	for i := 0; i < result.(model.Matrix).Len(); i++ {
+		namespaceValue, ok := result.(model.Matrix)[i].Metric[namespace]
+		if !ok {
+			continue
 		}
-		//Loop through the results for the workload and validate that contains the required labels and that the entity exists in the systems data structure once validated will write out the workload for the system.
-		for i := 0; i < result.(model.Matrix).Len(); i++ {
-			if namespaceValue, ok := result.(model.Matrix)[i].Metric[namespace]; ok {
-				if _, ok := systems[string(namespaceValue)]; ok {
-					if midValue, ok := result.(model.Matrix)[i].Metric[mid]; ok {
-						//fmt.Println(systems[string(namespaceValue)].mids[string(midValue)].midInfo)
-						//fmt.Println(namespaceValue, "-------", midValue)
-						//fmt.Println(result.(model.Matrix)[i].Metric[mid])
-						if _, ok := systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)]; ok { //NOT PASSING THIS STATMENT
-							for kc := range systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].containers {
-								//Loop through the different values over the interval and write out each one to the workload file.
-								for j := 0; j < len(result.(model.Matrix)[i].Values); j++ {
-									fmt.Fprintf(file, "%s,%s,%s,%s,%s,%s,%f\n", cluster, namespaceValue, systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].name, systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].kind, strings.Replace(string(kc), ":", ".", -1), time.Unix(0, int64(result.(model.Matrix)[i].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"), result.(model.Matrix)[i].Values[j].Value)
-								}
-							}
-						}
-					}
-				}
+		if _, ok := systems[string(namespaceValue)]; !ok {
+			continue
+		}
+		midValue, ok := result.(model.Matrix)[i].Metric[mid]
+		if !ok {
+			continue
+		}
+		if _, ok := systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)]; !ok { //NOT PASSING THIS STATMENT
+			continue
+		}
+		for kc := range systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].containers {
+			//Loop through the different values over the interval and write out each one to the workload file.
+			for j := 0; j < len(result.(model.Matrix)[i].Values); j++ {
+				fmt.Fprintf(file, "%s,%s,%s,%s,%s,%s,%f\n", cluster, namespaceValue, systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].name, systems[string(namespaceValue)].pointers[prefix+"__"+string(midValue)].kind, strings.Replace(string(kc), ":", ".", -1), time.Unix(0, int64(result.(model.Matrix)[i].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"), result.(model.Matrix)[i].Values[j].Value)
 			}
+
 		}
 	}
 }

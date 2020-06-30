9b0c6c2b6b408adc4240eaaf27b0437d3cfad4a8
Vlad <vilgovskiy@densify.com>
2020-06-30 11:46:35 -0400

    query node group metrics and write to respective files

--- internal/cluster/cluster.go
+++ internal/cluster/cluster.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"math"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/densify-dev/Container-Optimization-Data-Forwarder/internal/common"
@@ -81,6 +82,35 @@ func getWorkload(fileName, metricName, query string, args *common.Parameters) {
 	workloadWrite.Close()
 }
 
+func getNodeGroupWorkload(fileName, metricName, query string, metricfield model.LabelName, args *common.Parameters) {
+	var historyInterval time.Duration
+	historyInterval = 0
+	var result model.Value
+
+	//Open the files that will be used for the workload data types and write out there headers.
+	workloadWrite, err := os.Create("./data/node_group/" + fileName + ".csv")
+	if err != nil {
+		args.ErrorLogger.Println("metric=" + metricName + " query=" + query + " message=" + err.Error())
+		fmt.Println("metric=" + metricName + " query=" + query + " message=" + err.Error())
+		return
+	}
+	fmt.Fprintf(workloadWrite, "cluster,node_group,Datetime,%s\n", metricName)
+
+	//If the History parameter is set to anything but default 1 then will loop through the calls starting with the current day\hour\minute interval and work backwards.
+	//This is done as the farther you go back in time the slpwer prometheus querying becomes and we have seen cases where will not run from timeouts on Prometheus.
+	//As a result if we do hit an issue with timing out on Prometheus side we still can send the current data and data going back to that point vs losing it all.
+	for historyInterval = 0; int(historyInterval) < *args.History; historyInterval++ {
+		range5Min := prometheus.TimeRange(args, historyInterval)
+
+		result = prometheus.MetricCollect(args, query, range5Min, metricName, false)
+		if result != nil {
+			writeNodeGroupWorkload(workloadWrite, result, metricfield, args)
+		}
+	}
+	//Close the workload files.
+	workloadWrite.Close()
+}
+
 //writeWorkload will write out the workload data specific to metric provided to the file that was passed in.
 func writeWorkload(file io.Writer, result model.Value, args *common.Parameters) {
 	//Loop through the different values over the interval and write out each one to the workload file.
@@ -96,7 +126,31 @@ func writeWorkload(file io.Writer, result model.Value, args *common.Parameters) 
 			time.Unix(0, int64(result.(model.Matrix)[0].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"),
 			val)
 	}
+}
 
+//writeWorkload will write out the workload data specific to metric provided to the file that was passed in.
+func writeNodeGroupWorkload(file io.Writer, result model.Value, metricfield model.LabelName, args *common.Parameters) {
+	if result == nil {
+		return
+	}
+	//Loop through the results for the workload and validate that contains the required labels and that the entity exists in the systems data structure once validated will write out the workload for the system.
+	for i := 0; i < result.(model.Matrix).Len(); i++ {
+		nodeGroup, ok := result.(model.Matrix)[i].Metric[metricfield]
+		if !ok {
+			continue
+		}
+		//Loop through the different values over the interval and write out each one to the workload file.
+		for j := 0; j < len(result.(model.Matrix)[i].Values); j++ {
+			var val model.SampleValue = 0
+			if !math.IsNaN(float64(result.(model.Matrix)[i].Values[j].Value)) && !math.IsInf(float64(result.(model.Matrix)[i].Values[j].Value), 0) {
+				val = result.(model.Matrix)[i].Values[j].Value
+			}
+			fmt.Fprintf(file, "%s,%s,%s,%f\n",
+				*args.ClusterName, strings.Replace(string(nodeGroup), ";", ".", -1),
+				time.Unix(0, int64(result.(model.Matrix)[i].Values[j].Timestamp)*1000000).Format("2006-01-02 15:04:05.000"),
+				val)
+		}
+	}
 }
 
 //writeConfig will create the config.csv file that is will be sent Densify by the Forwarder.
@@ -213,4 +267,59 @@ func Metrics(args *common.Parameters) {
 	//Query and store prometheus Memory requests
 	query = `sum((kube_pod_container_resource_requests_memory_bytes/1024/1024) * on (namespace,pod,container) group_left kube_pod_container_status_running) / sum(kube_node_status_allocatable_memory_bytes/1024/1024) * 100`
 	getWorkload("memory_reservation_percent", "Memory Reservation Percent", query, args)
+
+	// Node group stuff
+	// AKS Query: avg((sum(kube_pod_container_resource_limits_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on  (node) group_left kube_node_labels{label_agentpool=~".+"}) by (label_agentpool)
+	// GKE Query: avg((sum(kube_pod_container_resource_limits_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on  (node) group_left kube_node_labels{label_cloud_google_com_gke_nodepool=~".+"}) by (label_cloud_google_com_gke_nodepool)
+	// EKS Query: avg((sum(kube_pod_container_resource_limits_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on  (node) group_left kube_node_labels{label_eks_amazonaws_com_nodegroup=~".+"}) by (label_eks_amazonaws_com_nodegroup)
+	// IKS Query: avg((sum(kube_pod_container_resource_limits_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on  (node) group_left kube_node_labels{label_pool_name=~".+"}) by (label_pool_name)
+	// Node group stuff
+
+	var nodeGroupLabel string = ""
+
+	var nodeGroups map[string][]string = make(map[string][]string)
+
+	query = `avg(kube_node_labels) by (label_cloud_google_com_gke_nodepool,label_eks_amazonaws_com_nodegroup, label_agentpool, label_pool_name)`
+	result = prometheus.MetricCollect(args, query, range5Min, "nodeGroupingLabelLookup", false)
+	if result == nil {
+		return
+	}
+
+	for i := range result.(model.Matrix) {
+		for labelName := range result.(model.Matrix)[i].Metric {
+			nodeGroupLabel = string(labelName)
+		}
+	}
+
+	if nodeGroupLabel == "" {
+		return
+	}
+
+	query = `kube_node_labels{` + nodeGroupLabel + `=~".+"}`
+	result = prometheus.MetricCollect(args, query, range5Min, "groupedNodes", false)
+	if result == nil {
+		return
+	}
+	for i := range result.(model.Matrix) {
+		nodeGroup := string(result.(model.Matrix)[i].Metric[model.LabelName(nodeGroupLabel)])
+		node := string(result.(model.Matrix)[i].Metric[`node`])
+		nodeGroups[nodeGroup] = append(nodeGroups[nodeGroup], node)
+	}
+	fmt.Println(nodeGroups)
+
+	query = `avg((sum(kube_pod_container_resource_limits_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on  (node) group_right kube_node_labels{` + nodeGroupLabel + `=~".+"}) by (` + nodeGroupLabel + `)`
+	result = prometheus.MetricCollect(args, query, range5Min, "nodeGroupCPULimit", false)
+	getNodeGroupWorkload("cpu_core_limit", "Average CPU Core Limit per Node Group", query, model.LabelName(nodeGroupLabel), args)
+
+	query = `avg((sum(kube_pod_container_resource_requests_cpu_cores * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)*1000) * on (node) group_right kube_node_labels{` + nodeGroupLabel + `=~".+"}) by (` + nodeGroupLabel + `)`
+	result = prometheus.MetricCollect(args, query, range5Min, "nodeGroupCPULimit", false)
+	getNodeGroupWorkload("cpu_core_request", "Average CPU Core Request per Node Group", query, model.LabelName(nodeGroupLabel), args)
+
+	query = `avg((sum(kube_pod_container_resource_limits_memory_bytes * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)/1024/1024) * on (node) group_right kube_node_labels{` + nodeGroupLabel + `=~".+"}) by (` + nodeGroupLabel + `)`
+	result = prometheus.MetricCollect(args, query, range5Min, "nodeGroupCPULimit", false)
+	getNodeGroupWorkload("memory_limit_bytes", "Average Memory Limit(Bytes) per Node Group", query, model.LabelName(nodeGroupLabel), args)
+
+	query = `avg((sum(kube_pod_container_resource_requests_memory_bytes * on (namespace,pod,container) group_left kube_pod_container_status_running) by (node)/1024/1024) * on (node) group_right kube_node_labels{` + nodeGroupLabel + `=~".+"}) by (` + nodeGroupLabel + `)`
+	result = prometheus.MetricCollect(args, query, range5Min, "nodeGroupCPULimit", false)
+	getNodeGroupWorkload("memory_request_bytes", "Average Memory Request(Bytes) per Node Group", query, model.LabelName(nodeGroupLabel), args)
 }

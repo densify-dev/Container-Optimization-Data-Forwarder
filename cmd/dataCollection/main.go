4376d45e6db2a4f4746148b12a0f134ead9ada66
kgillan <kgillan@densify.com>
2021-12-03 13:55:59 -0500

    Update version

--- cmd/dataCollection/main.go
+++ cmd/dataCollection/main.go
@@ -307,8 +307,8 @@ func main() {
 
 	//Read in the command line and config file parameters and set the required variables.
 	initParameters()
-	params.InfoLogger.Println("Version 2.3.0")
-	fmt.Println("[INFO] Version 2.3.0")
+	params.InfoLogger.Println("Version 2.4.0")
+	fmt.Println("[INFO] Version 2.4.0")
 
 	//Get the current time in UTC and format it. The script uses this time for all the queries this way if you have a large environment we are collecting the data as a snapshot of a specific time and not potentially getting a misaligned set of data.
 	var t time.Time
